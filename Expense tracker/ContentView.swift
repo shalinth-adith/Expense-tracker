81550d15b31e536992d2b2148b78a3efcbb4218e
shalinth-adith <shalinth.adith@outlook.com>
2025-06-17 21:03:00 +0530

    adding and deleting field

--- Expense tracker/ContentView.swift
+++ Expense tracker/ContentView.swift
@@ -4,20 +4,44 @@
 //
 //  Created by shalinth adithyan on 16/06/25.
 //
-
+import Observation
 import SwiftUI
 
+struct ExpenseItems  {
+    let name : String
+    let type : String
+    let amount : Double
+}
+@Observable
+class Expenses  {
+    var items = [ExpenseItems]()
+}
 struct ContentView: View {
+    @State private var expenses = Expenses()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach(expenses.items , id:\.name){item in
+                    Text(item.name)
+                }
+                .onDelete(perform: DeleteButton)
+            }
+            .navigationTitle("Expense_Tracker")
+            .toolbar{
+                Button("Add Expense ", systemImage:"plus"){
+                    let expense = ExpenseItems(name:"Test" , type :"Personal" , amount:5)
+                    expenses.items.append(expense)
+                    
+                }
+            }
+            }
         }
-        .padding()
+    func DeleteButton(at offsets:IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+        
     }
 }
+
 
 #Preview {
     ContentView()
